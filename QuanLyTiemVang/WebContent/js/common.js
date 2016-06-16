2f98caa1157a2a55630da8588028c6ed1833ce01
dongphuong88 <dongphuong88@yahoo.com>
2016-06-16 16:46:15 -0400

    Updated Trade functions using hidden area to solve Map Data structure in Javascript
    TODO: Using JSON
    TODO: Using Jquery template

--- QuanLyTiemVang/WebContent/js/common.js
+++ QuanLyTiemVang/WebContent/js/common.js
@@ -10,3 +10,40 @@ function getParam(name){
    if(name=(new RegExp('[?&]'+encodeURIComponent(name)+'=([^&]*)')).exec(location.search))
       return decodeURIComponent(name[1]);
 }
+
+// Show Accordions for w3
+function showAccordions(id) {
+    var x = document.getElementById(id);
+    if (x.className.indexOf("w3-show") == -1) {
+        x.className += " w3-show";
+    } else { 
+        x.className = x.className.replace(" w3-show", "");
+    }
+}
+
+// Change Goldweight to text (input : ly)
+function readGoldWeight( weightLy) {
+    var text = "";
+    var currWeight = weightLy;
+    var unit = currWeight % 10; // ly
+    if( unit > 0)
+        text = unit + " ly" + text;
+    currWeight = Math.floor(currWeight / 10);
+    unit = currWeight % 10; // phan
+    if( unit > 0)
+        text = unit + " phân " + text;
+    currWeight = Math.floor(currWeight / 10);
+    unit = currWeight % 10; // chi
+    if( unit > 0)
+        text = unit + " chỉ " + text;
+    currWeight = Math.floor(currWeight / 10);
+    unit = currWeight % 10; // luong
+    if( unit > 0)
+        text = unit + " lượng " + text;
+    return text;
+}
+
+// Check selectors exist
+$.fn.exists = function () {
+    return this.length !== 0;
+}
